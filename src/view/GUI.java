29a324ca405be693369feff789edf04bef230bcb
Adrien Nini Pereira <adrien97nini@gmail.com>
2017-12-17 00:53:41 +0100

    Create Gui file

---
+++ src/view/GUI.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package view;
+
+import java.util.Observable;
+
+import controller.ControllerNetwork;
+import model.ModelNetwork;
+
+/**
+ * @author Adrien
+ *
+ */
+public class GUI extends ViewNetwork{
+	
+	public GUI(ModelNetwork m, ControllerNetwork c) {
+		super(m, c);
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
